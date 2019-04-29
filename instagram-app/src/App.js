bb746fb5880446a6f2db63ede3a3e666bcdce39e
Seth Lachman <lachmanseth17@gmail.com>
2019-04-29 15:28:33 -0400

    Removed extra instagram folder and some boilerplate files. Added dummy-data to app.js file.

--- instagram-app/src/App.js
+++ instagram-app/src/App.js
@@ -1,26 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import dummyData from './dummy-data';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      data: dummyData
+    }
+  }
+
+  render() {
+    console.log(this.state)
+    return (
+      <div className="wrapper">
+        <h2>Hello World</h2>
+      </div>
+    )
+   
+  }
 }
 
 export default App;
