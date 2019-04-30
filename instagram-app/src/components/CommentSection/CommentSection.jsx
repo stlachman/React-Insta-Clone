c95f7c4a9a0294eb4edc0abb50ef27620fbd2c74
Seth Lachman <lachmanseth17@gmail.com>
2019-04-30 15:37:04 -0400

    Updated comment section component to class component.

--- instagram-app/src/components/CommentSection/CommentSection.jsx
+++ instagram-app/src/components/CommentSection/CommentSection.jsx
@@ -3,22 +3,33 @@ import Comment from './Comment';
 import PropTypes from 'prop-types';
 import './CommentSection.css';
 
-const CommentSection = (props) => {
-	return (
-    <div className="comment-container">
-      <button className="comment-icon"><i className="far fa-heart"></i></button><button className="comment-icon"><i className="far fa-comment"></i></button>
-      <p>{props.likes} likes</p>
-      {props.comments.map((comment, i) => {
-        return <Comment username={comment.username} text={comment.text} key={i}/>
-      })}
-      <p>{props.time}</p>
-      <form className="comment-form" action="#" method="POST">
-        <textarea className="comment-input" type="text" placeholder="Add a comment..">
-        </ textarea>
-      </form>
-    </div>
-  )
-};
+
+class CommentSection extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      input: ''
+    }
+  }
+
+  render() {
+    console.log(this.props);
+    return (
+      <div className="comment-container">
+        <button className="comment-icon"><i className="far fa-heart"></i></button><button className="comment-icon"><i className="far fa-comment"></i></button>
+        <p>{this.props.likes} likes</p>
+        {this.props.comments.map((comment, i) => {
+          return <Comment username={comment.username} text={comment.text} key={i}/>
+        })}
+        <p>{this.props.time}</p>
+        <form className="comment-form" action="#" method="POST">
+          <textarea className="comment-input" type="text" placeholder="Add a comment..">
+          </ textarea>
+        </form>
+      </div>
+    )
+  }
+}
 
 CommentSection.propTypes = {
   comments: PropTypes.array,
