a58c43c6bce4ba46e89b547a1684ff75e351c9d8
Seth Lachman <lachmanseth17@gmail.com>
2019-04-30 17:50:26 -0400

    Added function for search and converted search component to class component.

--- instagram-app/src/App.js
+++ instagram-app/src/App.js
@@ -5,52 +5,61 @@ import PostContainer from './components/PostContainer/PostContainer';
 import SearchBar from './components/SearchBar/SearchBar';
 
 class App extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      data: [],
-      comments: []
-    }
+	constructor() {
+		super();
+		this.state = {
+			data: [],
+			comments: []
+		};
+	}
+
+	componentDidMount() {
+		this.setState({
+			data: dummyData,
+			comments: dummyData.map((data) => data.comments)
+		});
+	}
+
+	addNewComment = (input, id) => {
+		this.setState({
+			data: this.state.data.map(
+				(post) =>
+					post.id === id
+						? { ...post, comments: [ ...post.comments, { text: input, username: 'Test User' } ] }
+						: post
+			)
+		});
+	};
+
+	addLike = (id) => {
+		this.setState({
+			data: this.state.data.map((post) => (post.id === id ? { ...post, likes: post.likes + 1 } : post))
+		});
+  };
+  
+  searchPosts = (query) => {
+    console.log(query)
   }
 
-  componentDidMount() {
-    this.setState({
-      data: dummyData,
-      comments: dummyData.map(data => data.comments) 
-
-    })
-  }
-
-  addNewComment = (input, id) => {
-    this.setState({
-      data: this.state.data.map(post =>
-        post.id === id ? { ...post, comments: [...post.comments, {text: input, username: 'Test User'}]  } : post
-      )
-    })
-  }
-
-  addLike = (id) => {
-    console.log(id);
-    this.setState({
-      data: this.state.data.map(post =>
-        post.id === id ? { ...post, likes: post.likes + 1 } : post
-      )
-    })
-  }
-
-  render() {
-    return (
-      <div className="wrapper">
-        <SearchBar />
-        <div className="posts">
-          {this.state.data.map(post => {
-            return <PostContainer onAddLike={this.addLike} onAddNewComment={this.addNewComment} post={post} key={post.id}/>
-          })}
-        </div>
-      </div>
-    )
-   
-  }
+	render() {
+		return (
+			<div className="wrapper">
+				<SearchBar onSearchPosts={this.searchPosts} />
+				<div className="posts">
+					{this.state.data.map((post) => {
+						return (
+							<PostContainer
+								onAddLike={this.addLike}
+								onAddNewComment={this.addNewComment}
+								post={post}
+								key={post.id}
+							/>
+						);
+					})}
+				</div>
+			</div>
+		);
+	}
 }
 
 export default App;
--- instagram-app/src/components/SearchBar/SearchBar.jsx
+++ instagram-app/src/components/SearchBar/SearchBar.jsx
@@ -1,22 +1,43 @@
 import React from 'react';
 import './SearchBar.css';
 
-const SearchBar = props => {
-  return (
-    <div className="searchbar">
-      <div className="logo">
-        <a href="https://www.instagram.com/"><i className="fab fa-instagram"></i></a> <div className="divider"></div> <a href="https://www.instagram.com/">Instagram</a>
+class SearchBar extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      search: ''
+    }
+  }
+
+  onUpdate = event => {
+    this.setState({[event.target.name]: event.target.value })
+  }
+
+  handleSubmit = event => {
+    event.preventDefault();
+    this.props.onSearchPosts(this.state.search);
+    this.setState({ search: "" });
+  }
+
+  render() {
+    return (
+      <div className="searchbar">
+        <div className="logo">
+          <a href="https://www.instagram.com/"><i className="fab fa-instagram"></i></a> <div className="divider"></div> <a href="https://www.instagram.com/">Instagram</a>
+        </div>
+        <div className="search-bar">
+          <form onSubmit={this.handleSubmit}>
+            <input type="text" placeholder="Search" name="search" onChange={this.onUpdate} value={this.state.search} />
+          </form>
+        </div>
+        <div className="user-info">
+          <button className="user-icon"><i className="far fa-compass"></i></button>
+          <button className="user-icon"><i className="far fa-heart"></i></button>
+          <button className="user-icon"><i className="far fa-user"></i></button>
+        </div>
       </div>
-      <div className="search-bar">
-        <input type="text" placeholder="Search" />
-      </div>
-      <div className="user-info">
-        <button className="user-icon"><i className="far fa-compass"></i></button>
-        <button className="user-icon"><i className="far fa-heart"></i></button>
-        <button className="user-icon"><i className="far fa-user"></i></button>
-      </div>
-    </div>
-  )
-};
+    )
+  }
+}
 
 export default SearchBar;
