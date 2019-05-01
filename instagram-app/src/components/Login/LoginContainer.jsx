5d1a1d8913f48b163fe7a2319e0bee0d9d7ecb27
Seth Lachman <lachmanseth17@gmail.com>
2019-05-01 18:56:48 -0400

    Added styled components package and first styled component.

--- instagram-app/src/components/Login/LoginContainer.jsx
+++ instagram-app/src/components/Login/LoginContainer.jsx
@@ -1,10 +1,21 @@
 import React from 'react';
 import LoginForm from './LoginForm';
+import styled from 'styled-components';
+
+const FormContainer = styled.div`
+  margin: 5rem auto 0;
+  max-width: 350px;
+  text-align: center;
+  background: #fff;
+  color: #262626;
+  border: 1px solid #e6e6e6;
+  padding: 0 0 4rem;
+`;
 
 const LoginContainer = (props) => {
 	return (
 		<section className="login">
-			<div className="login-form-container">
+			<FormContainer>
 				<h2>Instagram</h2>
 				<LoginForm
 					username={props.username}
@@ -12,7 +23,7 @@ const LoginContainer = (props) => {
 					onHandleChange={props.onHandleChange}
 					onHandleSubmit={props.onHandleSubmit}
 				/>
-			</div>
+      </FormContainer>
 		</section>
 	);
 };
